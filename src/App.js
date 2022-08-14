8b4f850da45053a1f7114b71d31f6b9c7adecb51
Mazoni <lipemazonisilva@gmail.com>
2022-08-13 23:58:11 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Equipe = (props) =>{
+    return(
+        <div>
+            <h1>Área: {props.nome}</h1>
+            <hr/>
+            <Membro nome="Carlos" cargo="Front-End Junior" idade="20"/>
+            <hr/>
+            <Membro nome="Amanda" cargo="Fullstack pleno" idade="27"/>
+            <hr/>
+            <Membro nome="Felipe" cargo="Back-End Junior" idade="22"/>
+            <hr/>
+            <Membro nome="Mauricio" cargo="Analista de dados" idade="30"/>
+            <hr/>
+        </div>
+    )
 }
 
-export default App;
+const Membro = (props) =>{
+    return(
+        <div>
+            <h3>Funcionário:{props.nome}</h3>
+            <h3>Cargo:{props.cargo}</h3>
+            <h3>idade:{props.idade}</h3>      
+        </div>
+    )
+}
+
+export default function App() {
+    return ( 
+        <div>
+            <h1>Conheça nossa equipe:</h1>
+            <Equipe nome="Tecnologia"/>
+        </div>
+    )
+}
